24eb05db9463f8bfb95261774e97e7029cd5eb8b
BevzoI <bevzo.i.n@gmail.com>
2024-04-05 09:48:58 +0200

    task friend

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import user from './data/user.json';
 import data from './data/data.json';
+import friends from './data/friends.json';
 import transactions from './data/transactions.json';
 
 import { App } from 'App';
 import Profile from './components/Profile/Profile';
 import Statistics from './components/Statistics/Statistics';
+import FriendList from './components/FriendList/FriendList';
 import TransactionHistory from 'components/TransactionHistory/TransactionHistory';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -23,6 +25,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       />
       <Statistics title="Upload stats" stats={data} />
       <Statistics stats={data} />
+      <FriendList friends={friends} />
       <TransactionHistory items={transactions} />
     </App>
   </React.StrictMode>
